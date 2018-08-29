7564cda3d4a21e4e62bd6389d1381a1fdfc51ba8
divya <tiwari.divya50@gmail.com>
2018-08-29 15:02:45 +0530

    removed BitmapUtils class and RSBlurProcessor class Removed renderscriptsupport from build.gradle(App)

--- app/src/main/java/com/vitartha/hisabkitab/Activities/NewLoginscreen.java
+++ app/src/main/java/com/vitartha/hisabkitab/Activities/NewLoginscreen.java
@@ -1,22 +1,12 @@
 package com.vitartha.hisabkitab.Activities;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
 import android.os.Build;
-import android.renderscript.RenderScript;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.ImageView;
+
 import android.widget.RelativeLayout;
 
-import com.vitartha.hisabkitab.Class.RSBlurProcessor;
 import com.vitartha.hisabkitab.R;
-
-import static com.vitartha.hisabkitab.Class.BitmapUtils.getBitmapFromView;
 
 public class NewLoginscreen extends AppCompatActivity {
 
